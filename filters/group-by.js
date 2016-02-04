ffa1e085f533058ab67651f46fb6954f05529e61
Randy Houlahan <randy.houlahan@cbd.int>
2016-02-03 22:26:05 -0500

    chore: ad extended route

--- filters/group-by.js
+++ filters/group-by.js
@@ -4,7 +4,7 @@ define(['app','moment'], function(app,moment) {
     //
     //
     //============================================================
-    app.filter('groupBy', ["underscore", "$parse", function(_, $parse) {
+    app.filter('groupBy', ["lodash", "$parse", function(_, $parse) {
         var cacheMap = {};
         return _.memoize(function(items, field) {
             var getter = $parse(field);
