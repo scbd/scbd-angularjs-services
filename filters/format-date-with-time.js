f26b940c85657ba47221a8ff357b60ee1036cad1
mattdias <matthew.dias@cbd.int>
2016-03-23 11:25:06 -0400

    update time to be consistent

--- filters/format-date-with-time.js
+++ filters/format-date-with-time.js
@@ -7,7 +7,7 @@ define(['app','moment'], function(app,moment) {
     app.filter("formatDateWithTime", function() {
         return function(date, formart) {
             if (formart === undefined)
-                formart = 'MM/DD/YYYY hh:mm';
+                formart = 'DD MMM YYYY hh:mm';
             return moment.utc(date).format(formart);
         };
     });
