0cfa0c49756d3755d3ab7009bc42729348cc2050
Philippe Leblond <philippe.leblond@cbd.int>
2016-04-07 18:49:45 -0400

    Fix typo

--- services/authentication.js
+++ services/authentication.js
@@ -2,7 +2,7 @@ define(['app', './apiUrl'], function(app) {
 
     app.factory('apiToken', ["$q", "$rootScope", "$window", "$document", "apiUrl", function($q, $rootScope, $window, $document, apiUrl) {
 
-        var domain = $document.location.hostname.replace(/^[^\.]+\./, '');
+        var domain = document.location.hostname.replace(/^[^\.]+\./, '');
 
         var pToken;
         var ACCOUNTS_URL = apiUrl.devAccountsUrl() || 'https://accounts.'+domain;
